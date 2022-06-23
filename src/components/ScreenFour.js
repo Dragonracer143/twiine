0b264ef62f054c047f489bd9f1dbbd313ff9209e
unknown <sicsdev21@gmail.com>
2022-06-23 22:45:39 +0530

    activity screen

--- src/components/ScreenFour.js
+++ src/components/ScreenFour.js
@@ -8,17 +8,23 @@ const ScreenFour = (props) => {
       }
 
 
-     const activityType= (e)=>{
-      const value= e.target.value;
+     const activityType= (value)=>{
       props.setActivity(value);
      };
+     const setSelectedDiv=(value)=>{
+      if(value===props.activity){
+        return "showSpanSelected"
+      }else{
+        return "showSpan"
+      }
+     }
 
 console.log("act:", props.activity)
 
 
   return (
     <div className="Screen_4">
-    <div className="inner">
+    <div className="inner activityChoice">
       <div className="content">
         <h1>4. What kind of activities would you like to do?</h1>
         <p>
@@ -26,8 +32,32 @@ console.log("act:", props.activity)
         </p>
       </div>
       <div className="Activity_Choice">
-      <p>Activity Choice </p>
-      <form className="form">
+      <p>Energy-Level Choice </p>
+      <div className='showSpanMaster '>
+      <span className={setSelectedDiv("Low Energy")}
+      onClick={()=>{
+        activityType('Low Energy')
+      }}
+      >Low Energy</span>
+      <span className={setSelectedDiv("Mid Energy")}
+      onClick={()=>{
+        activityType('Mid Energy')
+      }}
+      >Mid Energy</span>
+      <span className={setSelectedDiv("High Energy")}
+      onClick={()=>{
+        activityType('High Energy')
+      }}
+      >High Energy</span>
+      <span className={setSelectedDiv("Adrenaline Junky")}
+      onClick={()=>{
+        activityType('Adrenaline Junky')
+      }}
+      >Adrenaline Junky</span>
+      </div>
+        
+      {/* <form className="form">
+        
         <select
          className="Activity_choice_dropdown" 
          name="Activity" 
@@ -39,8 +69,21 @@ console.log("act:", props.activity)
           <option >{ele?.activity}</option>    
           )}
         </select>
-        <button onClick={handleClick} type="button"><img src="./img/Arrow_button.png" alt="" /></button>
-      </form>
+      </form> */}
+      <button onClick={handleClick} type="button"><img src="./img/Arrow_button.png" alt="" /></button>
+      <div className='activityDescDiv'> 
+        <span className='activityDescDivHeading'> Energy-Level Key</span>
+        <div className='activityDescDivKeys'>
+        <span className='heading'>Low Energy</span>
+        <span className='desc'>Activity Like: Walking, Sightseeing etc.</span>
+        <span className='heading'>Mid Energy</span>
+        <span className='desc'>Activity Like:Bowling,Mini Golf,Escape Roops etc.</span>
+        <span className='heading'>High Energy</span>
+        <span className='desc'>Activity Like:Playing sports, Hiking etc.</span>
+        <span className='heading'>Adrenaline Junky</span>
+        <span className='desc'>Activity Like:Paragliding Skydiving etc.</span>
+        </div>
+      </div>
     </div>
     </div>
   </div>
