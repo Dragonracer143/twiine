5c8096dea915875c6efeda7c43843642a35faa78
sicsdev <sicsdev21@gmail.com>
2022-10-06 02:05:24 +0530

    latest change

--- src/components/spotify/MusicLogin.js
+++ src/components/spotify/MusicLogin.js
@@ -19,14 +19,14 @@ const Musiclogin = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const search = window.location.search;
-    let tokenapple = window.localStorage.getItem("id_token");
+    let tokenapple = window.localStorage.getItem("code");
     if (!tokenapple && search) {
 
       tokenapple = search
         .substring(2)
-        .split("&")
-        .find((elem) => elem.startsWith("id_token"))
-        .split("=")[2];
+        .split("?")
+        .find((elem) => elem.startsWith("code"))
+        .split("=")[1];
 
       window.location.search = "";
       localStorage.setItem("code", tokenapple);
