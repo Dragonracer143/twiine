8c7e3db61cb15aecd59d4db9a96e4ba877e13102
Vikas <vikas@joinnextmed.com>
2022-10-31 22:06:31 +0530

    latest updates

--- src/components/spotify/Userlocation.js
+++ src/components/spotify/Userlocation.js
@@ -1,37 +1,12 @@
 import React, { useEffect, useState } from "react";
 import useGeolocation from "react-hook-geolocation";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-const MusicKit = window.MusicKit;
 const Userlocation = (props) => {
   /* Get current location */
   let token = localStorage.getItem("id_token");
   let appletoken = localStorage.getItem("musicUsertoken");
 
-  const onGetdata = async (e) => {
-  axios
-      .get("https://api.music.apple.com/v1/me/storefront", {
-        headers: {
-          "Access-Control-Allow-Origin": "https://twine-new.vercel.app/",
-          "Access-Control-Allow-Methods":
-            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-          Authorization:
-            "Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllIS0xLSk5ZRDMifQ.eyJpYXQiOjE2NjUxNjg0MTIsImV4cCI6MTY4MDcyMDQxMiwiaXNzIjoiTllMVDdCVzg3UiJ9.qM3UV0c7KZiEXMVGkEWXgkEiEcP52WiMz_z71zMD5vnX6V1zOnZJl0jN9VH_4niJnzbYV_s9MhvWwmkC0h29bw",
-            "Music-User-Token": `${appletoken}`,
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log("response", response)
-      })
-      .catch((err) => {
-        console.log("eroor", err);
-      });
-  };
 
-  useEffect(() => {
-    onGetdata();
-  }, []);
 
   const geolocation = useGeolocation();
   const navigate = useNavigate();
