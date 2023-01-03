ef316bf6ad8767338036dd38ef5759c2775c6cad
sicsdev <vikas@joinnextmed.com>
2023-01-03 15:17:04 -0800

    latest updates

--- src/Services/Config.js
+++ src/Services/Config.js
@@ -1,10 +1,10 @@
 //Live Url
 //
-// export const baseUrl = process.env.REACT_APP_API_BASE_URL;
+export const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 // Local URL
 
-export const baseUrl = "http://localhost:8000/"
+// export const baseUrl = "http://localhost:8000/"
 
 // spotify api data
 
@@ -12,7 +12,7 @@ export const baseUrl = "http://localhost:8000/"
 
 export const scopes = [process.env.REACT_APP_SPOTIFY_SCOPES];
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_SPOTIFY_ID;
-export const REDIRECT_URI = "http://localhost:3000/";
-// export const REDIRECT_URI  = process.env.REACT_APP_REDIRECT_URI;
+// export const REDIRECT_URI = "http://localhost:3000/";
+export const REDIRECT_URI  = process.env.REACT_APP_REDIRECT_URI;
 export const AUTH_ENDPOINT = process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT;
 export const RESPONSE_TYPE = process.env.REACT_APP_SPOTFY_RESPONSE_TYPE
