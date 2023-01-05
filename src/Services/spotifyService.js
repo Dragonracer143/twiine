8a0dca37a115ae6a4d92bb61067a6f3565918b0d
sicsdev <vikas@joinnextmed.com>
2023-01-05 13:12:27 -0800

    fix

--- src/Services/spotifyService.js
+++ src/Services/spotifyService.js
@@ -3,8 +3,8 @@ import { SPOTIFY_REFRESH_TOKEN, SECRET_ID, CLIENT_ID } from "./Config";
 const axiosApiInstance = axios.create();
 
 const refreshAccessToken = async () => {
-  const basic = btoa(`${CLIENT_ID}:${SECRET_ID}`);
 
+  const basic = btoa(`${CLIENT_ID}:${SECRET_ID}`);
   // header paremeter
   const config = {
     headers: {
@@ -37,8 +37,10 @@ axiosApiInstance.interceptors.response.use(
       localStorage.setItem("token", access_token.access_token.toString());
       axios.defaults.headers.common["Authorization"] =
         "Bearer " + access_token.access_token;
+
       return axiosApiInstance(originalRequest);
     }
+    window.location.reload();
     return Promise.reject(error);
   }
 );
